fbead2128604053dc60556e8b981d47de37f6ea6
john.ellis@rsmart.com <john.ellis@rsmart.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2005-12-01 01:49:00 +0000

    http://bugs.osportfolio.org/jira/browse/OSP-725 OSP-725 createing data warehouse schedule job
    git-svn-id: https://source.sakaiproject.org/svn/trunk/osp@4339 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ osp-2.1/warehouse/api-impl/src/java/org/theospi/portfolio/warehouse/impl/DataWarehouseManagerImpl.java
@@ -0,0 +1,65 @@
+/**********************************************************************************
+* $URL$
+* $Id$
+***********************************************************************************
+*
+* Copyright (c) 2003, 2004 The Regents of the University of Michigan, Trustees of Indiana University,
+*                  Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
+*
+* Licensed under the Educational Community License Version 1.0 (the "License");
+* By obtaining, using and/or copying this Original Work, you agree that you have read,
+* understand, and will comply with the terms and conditions of the Educational Community License.
+* You may obtain a copy of the License at:
+*
+*      http://cvs.sakaiproject.org/licenses/license_1_0.html
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+* AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+* DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+**********************************************************************************/
+package org.theospi.portfolio.warehouse.impl;
+
+import org.theospi.portfolio.warehouse.intf.DataWarehouseManager;
+import org.theospi.portfolio.warehouse.intf.WarehouseTask;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.util.List;
+import java.util.Iterator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: John Ellis
+ * Date: Nov 30, 2005
+ * Time: 4:48:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DataWarehouseManagerImpl implements DataWarehouseManager {
+
+   private List tasks;
+
+   public void registerTask(WarehouseTask task) {
+      getTasks().add(task);
+   }
+
+   public void execute(JobExecutionContext jobExecutionContext)
+         throws JobExecutionException {
+
+      for (Iterator i=getTasks().iterator();i.hasNext();) {
+         WarehouseTask task = (WarehouseTask)i.next();
+         task.execute();
+      }
+   }
+
+   public List getTasks() {
+      return tasks;
+   }
+
+   public void setTasks(List tasks) {
+      this.tasks = tasks;
+   }
+
+}
