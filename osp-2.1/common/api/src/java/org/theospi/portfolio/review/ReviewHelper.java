1e9be5624477c3bdaeef353e9999598b69485169
chmaurer@iupui.edu <chmaurer@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-01-11 14:19:44 +0000

    Remove unused constants.
    git-svn-id: https://source.sakaiproject.org/svn/trunk/osp@5243 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- osp-2.1/common/api/src/java/org/theospi/portfolio/review/ReviewHelper.java
+++ osp-2.1/common/api/src/java/org/theospi/portfolio/review/ReviewHelper.java
@@ -10,22 +10,7 @@ public interface ReviewHelper {
    
    public static final String REVIEW_PARENT =
       "org.theospi.portfolio.review.parent";
-   
-   public static final String REVIEW_TITLE = 
-      "org.theospi.portfolio.review.title_key";
-   
-   public static final String REVIEW_INSTRUCTIONS = 
-      "org.theospi.portfolio.review.instructions_key";
-   
-   public static final String REVIEW_VISIBILITY_LABEL = 
-      "org.theospi.portfolio.review.visibility_label_key";
-   
-   public static final String REVIEW_FORM_LABEL = 
-      "org.theospi.portfolio.review.form_label_key";
-   
-   public static final String REVIEW_MANAGE_CONTENT = 
-      "org.theospi.portfolio.review.manage_content_key";
-   
+  
    public static final String REVIEW_BUNDLE_PREFIX = 
       "org.theospi.portfolio.review.bundle_prefix";
    
