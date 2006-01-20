cb415ec3f8197b7c0fd15c00b1c1bd7d35ab5620
andersjb@iupui.edu <andersjb@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-01-20 22:57:13 +0000

    From Mamta
    git-svn-id: https://source.sakaiproject.org/svn/trunk/osp@5469 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ osp-2.1/warehouse/api-impl/src/java/org/theospi/portfolio/warehouse/osp/matrix/ScaffoldingWarehouseTask.java
@@ -0,0 +1,54 @@
+/**********************************************************************************
+* $URL: https://source.sakaiproject.org/svn/trunk/osp/osp-2.1/warehouse/api-impl/src/java/org/theospi/portfolio/warehouse/osp/presentation/PresentationWarehouseTask.java $
+* $Id: PresentationWarehouseTask.java 4758 2005-12-19 06:14:01Z john.ellis@rsmart.com $
+***********************************************************************************
+*
+* Copyright (c) 2003, 2004 The Regents of the University of Michigan, Trustees of Indiana University,
+*                  Board of Trustees of the Leland Stanford, Jr., University, and The MIT Corporation
+*
+* Licensed under the Educational Community License Version 1.0 (the "License");
+* By obtaining, using and/or copying this Original Work, you agree that you have read,
+* understand, and will comply with the terms and conditions of the Educational Community License.
+* You may obtain a copy of the License at:
+*
+*      http://cvs.sakaiproject.org/licenses/license_1_0.html
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+* AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+* DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+**********************************************************************************/
+package org.theospi.portfolio.warehouse.osp.scaffolding;
+
+import org.theospi.portfolio.warehouse.impl.BaseWarehouseTask;
+import org.theospi.portfolio.matrix.MatrixManager;
+
+import java.util.Collection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: John Ellis
+ * Date: Dec 1, 2005
+ * Time: 10:49:22 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ScaffoldingWarehouseTask extends BaseWarehouseTask {
+
+   private MatrixManager matrixManager;
+   
+   protected Collection getItems() {
+	      return getMatrixManager().getScaffolding();
+	   }
+
+
+  
+   public MatrixManager getMatrixManager() {
+      return matrixManager;
+   }
+
+   public void setMatrixManager(MatrixManager matrixManager) {
+      this.matrixManager = matrixManager;
+   }
+}
