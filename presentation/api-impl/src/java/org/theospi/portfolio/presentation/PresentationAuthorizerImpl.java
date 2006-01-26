239bc4aed3a1dc95b19b7af30767d75abcd78ee0
john.ellis@rsmart.com <john.ellis@rsmart.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-01-26 06:02:52 +0000

    updating license
    git-svn-id: https://source.sakaiproject.org/svn/trunk/osp/osp-2.1@5557 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- presentation/api-impl/src/java/org/theospi/portfolio/presentation/PresentationAuthorizerImpl.java
+++ presentation/api-impl/src/java/org/theospi/portfolio/presentation/PresentationAuthorizerImpl.java
@@ -1,8 +1,23 @@
-/*
- * $Header: /opt/CVS/osp2.x/presentation/api-impl/src/java/org/theospi/portfolio/presentation/PresentationAuthorizerImpl.java,v 1.3 2005/09/15 17:37:27 jellis Exp $
- * $Revision$
- * $Date$
- */
+/**********************************************************************************
+* $URL$
+* $Id$
+***********************************************************************************
+*
+* Copyright (c) 2005, 2006 The Sakai Foundation.
+*
+* Licensed under the Educational Community License, Version 1.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.opensource.org/licenses/ecl1.php
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+**********************************************************************************/
 package org.theospi.portfolio.presentation;
 
 import org.theospi.portfolio.security.app.ApplicationAuthorizer;
--- wizard/api-impl/src/java/org/theospi/portfolio/wizard/WizardAuthorizerImpl.java
+++ wizard/api-impl/src/java/org/theospi/portfolio/wizard/WizardAuthorizerImpl.java
@@ -1,8 +1,23 @@
-/*
- * $Header: /opt/CVS/osp2.x/presentation/api-impl/src/java/org/theospi/portfolio/presentation/PresentationAuthorizerImpl.java,v 1.3 2005/09/15 17:37:27 jellis Exp $
- * $Revision: 3898 $
- * $Date: 2005-11-16 12:16:20 -0500 (Wed, 16 Nov 2005) $
- */
+/**********************************************************************************
+* $URL: https://source.sakaiproject.org/svn/trunk/sakai/legacy/component/src/java/org/sakaiproject/component/framework/email/TestEmailService.java $
+* $Id: TestEmailService.java 3831 2005-11-14 20:17:24Z ggolden@umich.edu $
+***********************************************************************************
+*
+* Copyright (c) 2005, 2006 The Sakai Foundation.
+*
+* Licensed under the Educational Community License, Version 1.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.opensource.org/licenses/ecl1.php
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+**********************************************************************************/
 package org.theospi.portfolio.wizard;
 
 import org.theospi.portfolio.security.app.ApplicationAuthorizer;
