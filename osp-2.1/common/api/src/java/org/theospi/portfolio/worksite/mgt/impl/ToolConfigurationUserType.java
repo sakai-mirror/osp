57ae85ab18b2437ebc86d66715d731242f3e5026
john.ellis@rsmart.com <john.ellis@rsmart.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-01-26 06:02:52 +0000

    updating license
    git-svn-id: https://source.sakaiproject.org/svn/trunk/osp@5557 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- osp-2.1/common/api/src/java/org/theospi/portfolio/worksite/mgt/impl/ToolConfigurationUserType.java
+++ osp-2.1/common/api/src/java/org/theospi/portfolio/worksite/mgt/impl/ToolConfigurationUserType.java
@@ -1,8 +1,23 @@
-/*
- * $Header: /opt/CVS/osp2.x/common/api/src/java/org/theospi/portfolio/worksite/mgt/impl/ToolConfigurationUserType.java,v 1.2 2005/08/11 02:58:35 jellis Exp $
- * $Revision$
- * $Date$
- */
+/**********************************************************************************
+* $URL$
+* $Id$
+***********************************************************************************
+*
+* Copyright (c) 2005, 2006 The Sakai Foundation.
+*
+* Licensed under the Educational Community License, Version 1.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.opensource.org/licenses/ecl1.php
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+**********************************************************************************/
 package org.theospi.portfolio.worksite.mgt.impl;
 
 import net.sf.hibernate.UserType;
