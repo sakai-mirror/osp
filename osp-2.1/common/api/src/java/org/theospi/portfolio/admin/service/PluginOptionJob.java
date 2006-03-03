0395229b3a688e81355b8aa45b285ed091dad26c
john.ellis@rsmart.com <john.ellis@rsmart.com@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-03-03 07:28:51 +0000

    http://bugs.osportfolio.org/jira/browse/OSP-1183 adding tools for portfolio program admin site type
    git-svn-id: https://source.sakaiproject.org/svn/trunk/osp@6416 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ osp-2.1/common/api/src/java/org/theospi/portfolio/admin/service/PluginOptionJob.java
@@ -0,0 +1,68 @@
+package org.theospi.portfolio.admin.service;
+
+import org.quartz.*;
+import org.theospi.portfolio.admin.intf.SakaiIntegrationPlugin;
+import org.theospi.portfolio.admin.model.IntegrationOption;
+import org.sakaiproject.api.app.scheduler.SchedulerManager;
+import org.sakaiproject.api.kernel.session.cover.SessionManager;
+import org.sakaiproject.api.kernel.component.cover.ComponentManager;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.Iterator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: John Ellis
+ * Date: Mar 2, 2006
+ * Time: 10:52:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PluginOptionJob implements Job {
+
+   public final static String PLUGIN = "org.theospi.portfolio.admin.service.PluginOptionJob.job";
+   public final static String INTERVAL = "org.theospi.portfolio.admin.service.PluginOptionJob.interval";
+
+   public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+      org.sakaiproject.api.kernel.session.Session sakaiSession = SessionManager.getCurrentSession();
+      String userId = sakaiSession.getUserId();
+
+      try {
+         sakaiSession.setUserId("admin");
+         sakaiSession.setUserEid("admin");
+         Map details = jobExecutionContext.getJobDetail().getJobDataMap();
+         SchedulerManager manager = (SchedulerManager) ComponentManager.get(SchedulerManager.class);
+         String pluginId = (String) details.get(PLUGIN);
+         SakaiIntegrationPlugin plugin = (SakaiIntegrationPlugin) ComponentManager.get(pluginId);
+         long millis = Long.parseLong((String) details.get(INTERVAL));
+
+         boolean done = true;
+         for (Iterator i=plugin.getPotentialIntegrations().iterator();i.hasNext();) {
+            if (!plugin.executeOption((IntegrationOption) i.next())) {
+               done = false;
+               break;
+            }
+         }
+
+         if (!done) {
+            try {
+               schedule(manager, pluginId, millis);
+            } catch (SchedulerException e) {
+               throw new JobExecutionException(e);
+            }
+         }
+      } finally {
+         sakaiSession.setUserEid(userId);
+         sakaiSession.setUserId(userId);
+      }
+   }
+
+   public static void schedule(SchedulerManager manager, String pluginId, long millis) throws SchedulerException {
+      JobDetail detail = new JobDetail(pluginId + "." + System.currentTimeMillis(),
+         PluginOptionJob.class.toString(), PluginOptionJob.class);
+      detail.getJobDataMap().put(PLUGIN, pluginId);
+      detail.getJobDataMap().put(INTERVAL, millis + "");
+      manager.getScheduler().scheduleJob(detail, new SimpleTrigger(pluginId + ".trigger." + System.currentTimeMillis(),
+         PluginOptionJob.class.toString(), new Date(System.currentTimeMillis() + millis)));
+   }
+}
