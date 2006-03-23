c9c30be54f36e04fceb497c7926be08b1d2812fc
andersjb@iupui.edu <andersjb@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-03-23 22:07:00 +0000

    http://bugs.osportfolio.org/jira/browse/OSP-1298
    git-svn-id: https://source.sakaiproject.org/svn/trunk/osp/osp-2.1@6968 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ warehouse/api-impl/src/java/org/theospi/portfolio/warehouse/osp/style/StyleWarehouseTask.java
@@ -0,0 +1,54 @@
+/**********************************************************************************
+* $URL: https://source.sakaiproject.org/svn/trunk/osp/osp-2.1/warehouse/api-impl/src/java/org/theospi/portfolio/warehouse/sakai/resource/ContentResourceWarehouseTask.java $
+* $Id: ContentResourceWarehouseTask.java 5557 2006-01-26 06:02:52Z john.ellis@rsmart.com $
+***********************************************************************************
+*
+* Copyright (c) 2005, 2006 The Sakai Foundation.
+*
+* Licensed under the Educational Community License, Version 1.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.opensource.org/licenses/ecl1.php
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+**********************************************************************************/
+package org.theospi.portfolio.warehouse.osp.style;
+
+import org.theospi.portfolio.warehouse.impl.BaseWarehouseTask;
+import org.theospi.portfolio.style.mgt.StyleManager;
+import java.util.Collection;
+import java.util.List;
+import java.util.Iterator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: John Ellis
+ * Date: Dec 19, 2005
+ * Time: 12:38:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StyleWarehouseTask extends BaseWarehouseTask {
+
+   private StyleManager styleManager;
+
+   protected Collection getItems() {
+      Collection styles = getStyleManager().getStylesForWarehouse();
+      
+      return styles;
+   }
+
+   public StyleManager getStyleManager() {
+      return styleManager;
+   }
+
+   public void setStyleManager(StyleManager styleManager) {
+      this.styleManager = styleManager;
+   }
+
+}
