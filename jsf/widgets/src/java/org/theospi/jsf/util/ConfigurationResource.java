7105b6bdd343b66081f384a621dde3276e2a1e23
chmaurer@iupui.edu <chmaurer@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-05-08 19:38:38 +0000

    Fixing the svn tag for url and id.
    git-svn-id: https://source.sakaiproject.org/svn/osp/trunk@9125 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- jsf/widgets/src/java/org/theospi/jsf/util/ConfigurationResource.java
+++ jsf/widgets/src/java/org/theospi/jsf/util/ConfigurationResource.java
@@ -28,7 +28,7 @@ import java.util.Locale;
 /**
  * Thin wrapper for lookup of configuration of resources.
  * @author Ed Smiley
- * @version $Id$
+ * @version $Id: $
  */
 public class ConfigurationResource
 {
