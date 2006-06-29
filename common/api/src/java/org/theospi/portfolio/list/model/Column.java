0b92de95c60be0ef97e18ce621d03871005b160e
chmaurer@iupui.edu <chmaurer@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-06-29 14:58:30 +0000

    There were some weird characters in the header from a previous commit.  Hopefully this will remove them!
    git-svn-id: https://source.sakaiproject.org/svn/osp/trunk@11372 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- common/api/src/java/org/theospi/portfolio/list/model/Column.java
+++ common/api/src/java/org/theospi/portfolio/list/model/Column.java
@@ -9,7 +9,7 @@
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
 *
-*Ê Ê Ê http://www.opensource.org/licenses/ecl1.php
+*      http://www.opensource.org/licenses/ecl1.php
 *
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
