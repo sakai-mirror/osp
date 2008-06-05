e2edd51f32ea9e10b5dd6da78c107d60abe8ea16
chmaurer@iupui.edu <chmaurer@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-06-05 13:12:00 +0000

    Changes to the script
    git-svn-id: https://source.sakaiproject.org/svn/osp/branches/oncourse_osp_enhancements@47127 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- osp/sql/mysql/osp_oncourse.sql
+++ osp/sql/mysql/osp_oncourse.sql
@@ -12,9 +12,26 @@ ALTER TABLE osp_scaffolding Drop COLUMN reviewerGroupAccess;
 ALTER TABLE osp_scaffolding ADD COLUMN (defaultFormsMatrixVersion boolean);
 UPDATE osp_scaffolding SET defaultFormsMatrixVersion = true
 
-update osp_wizard_page_def as wpd set wpd.type = '1';
+alter table osp_wizard_page_def add column (type varchar(1) default '0');
+--update osp_wizard_page_def as wpd set wpd.type = '1';
 update osp_wizard_page_def as wpd set wpd.type = '0' where wpd.id in (
 select distinct s.wiz_page_def_id From osp_scaffolding_cell s );
+
+update osp_wizard_page_def as wpd set wpd.type = '1' where wpd.id in (
+select distinct wps.wiz_page_def_id
+From osp_wizard w
+join osp_wizard_category wc on wc.wizard_id = w.id
+join osp_wizard_page_sequence wps on wps.category_id = wc.id
+where w.wizard_type = 'org.theospi.portfolio.wizard.model.Wizard.hierarchical'
+);
+
+update osp_wizard_page_def as wpd set wpd.type = '2' where wpd.id in (
+select distinct wps.wiz_page_def_id 
+From osp_wizard w
+join osp_wizard_category wc on wc.wizard_id = w.id
+join osp_wizard_page_sequence wps on wps.category_id = wc.id
+where w.wizard_type = 'org.theospi.portfolio.wizard.model.Wizard.sequential'
+);
 
 update TAGGABLE_LINK set TAG_CRITERIA_REF = concat(TAG_CRITERIA_REF,  '/0') where TAG_CRITERIA_REF like '/ospWizPageDef/%'
 
