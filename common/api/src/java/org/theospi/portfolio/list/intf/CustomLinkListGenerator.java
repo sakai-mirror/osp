880132e7b8022c9ab4423eb06e8508a2ae919631
csev@umich.edu <csev@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-08-05 12:20:35 +0000

    SAK-12938
    git-svn-id: https://source.sakaiproject.org/svn/osp/trunk@50092 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- common/api/src/java/org/theospi/portfolio/list/intf/CustomLinkListGenerator.java
+++ common/api/src/java/org/theospi/portfolio/list/intf/CustomLinkListGenerator.java
@@ -3,7 +3,21 @@
  * $Header: /opt/CVS/osp/src/portfolio/org/theospi/portfolio/list/intf/CustomLinkListGenerator.java,v 1.2 2005/08/30 21:27:09 jellis Exp $
  *
  ***********************************************************************************
- * Copyright (c) 2005 the r-smart group, inc.
+ *
+ * Copyright 2006 Sakai Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ *
+ *       http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
  **********************************************************************************/
 package org.theospi.portfolio.list.intf;
 
