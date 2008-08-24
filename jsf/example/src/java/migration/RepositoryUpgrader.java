85b2e83b2c2b7f35b61196ddaaba3d68be6674d0
csev@umich.edu <csev@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-08-24 07:47:04 +0000

    SAK-12938
    git-svn-id: https://source.sakaiproject.org/svn/osp/trunk@51324 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- jsf/example/src/java/migration/RepositoryUpgrader.java
+++ jsf/example/src/java/migration/RepositoryUpgrader.java
@@ -1,26 +1,8 @@
 /**********************************************************************************
-
 * $URL$
-
 * $Id$
-
 ***********************************************************************************
-
-*
-
-* Copyright (c) 2005, 2006 The Sakai Foundation.
-
-*
-
-* Licensed under the Educational Community License, Version 1.0 (the "License");
-
-* you may not use this file except in compliance with the License.
-
-* You may obtain a copy of the License at
-
-*
-
-*      http://www.opensource.org/licenses/ecl1.php
+ *
  * Copyright (c) 2006 Sakai Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
@@ -34,8 +16,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*
-
+ *
 **********************************************************************************/
 
 package migration;
