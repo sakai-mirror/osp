8ec791d3761eb72b1db0941084e3ad3abb5ad17d
bkirschn@umich.edu <bkirschn@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2009-12-08 17:40:05 +0000

    SAK-17508 roll back r69571 changes (made no difference)
    git-svn-id: https://source.sakaiproject.org/svn/osp/trunk@69575 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- presentation/api-impl/src/java/org/theospi/portfolio/presentation/model/impl/PresentationManagerImpl.java
+++ presentation/api-impl/src/java/org/theospi/portfolio/presentation/model/impl/PresentationManagerImpl.java
@@ -600,7 +600,6 @@ public class PresentationManagerImpl extends HibernateDaoSupport
          presentation.setOwner(getAuthnManager().getAgent());
       }
 
-      // creating new presentation
       if (presentation.isNewObject()) {
          if (updateDates || presentation.getCreated() == null) {
             presentation.setCreated(new Date(System.currentTimeMillis()));
@@ -617,38 +616,13 @@ public class PresentationManagerImpl extends HibernateDaoSupport
          }
          getHibernateTemplate().save(presentation);
          eventService.postEvent(EventConstants.EVENT_PORTFOLIO_ADD, presentation.getId().getValue());
-      } 
-      
-      // saving modified presentation
-      else {
+      } else {
          if (checkAuthz) {
             getAuthzManager().checkPermission(PresentationFunctionConstants.EDIT_PRESENTATION,
                presentation.getId());
          }
-         
-         // In order to mitigate timing issues with save/refresh:
-         //    - FlushMode is set to FLUSH_EAGER 
-         //    - auto-commit is set to true
-         try {
-            int oldFlushMode = getHibernateTemplate().getFlushMode();
-            boolean oldAutoCommit = getSession().connection().getAutoCommit();
-            try {
-               getHibernateTemplate().setFlushMode(getHibernateTemplate().FLUSH_EAGER);
-               getSession().connection().setAutoCommit(true); 
-               getHibernateTemplate().merge(presentation);
-               eventService.postEvent(EventConstants.EVENT_PORTFOLIO_REVISE, presentation.getId().getValue());
-            }
-            catch (Exception e ) {
-               logger.warn(e);
-            }
-            finally {
-               getHibernateTemplate().setFlushMode(oldFlushMode);
-               getSession().connection().setAutoCommit(oldAutoCommit); 
-            }
-         }
-         catch (Exception e ) {
-            logger.warn(e);
-         }
+         getHibernateTemplate().merge(presentation);
+         eventService.postEvent(EventConstants.EVENT_PORTFOLIO_REVISE, presentation.getId().getValue());
       }
       
       storePresentationPages(presentation.getPages(), presentation.getId());
