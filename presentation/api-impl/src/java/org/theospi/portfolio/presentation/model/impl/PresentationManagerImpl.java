2dc4efeff48b309c646deb27f293f1eaa36d48a3
bkirschn@umich.edu <bkirschn@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2010-01-11 21:57:30 +0000

    SAK-16045 svn merge -c 70902 https://source.sakaiproject.org/svn/osp/trunk
    git-svn-id: https://source.sakaiproject.org/svn/osp/branches/sakai_2-6-x@71374 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- presentation/api-impl/src/java/org/theospi/portfolio/presentation/model/impl/PresentationManagerImpl.java
+++ presentation/api-impl/src/java/org/theospi/portfolio/presentation/model/impl/PresentationManagerImpl.java
@@ -2506,7 +2506,7 @@ public class PresentationManagerImpl extends HibernateDaoSupport
       List pages = getHibernateTemplate().findByNamedQuery("findPortfolioPagesByPortfolioAndSequence", 
             new Object[]{presentationId, new Integer(pageIndex)});
 
-      return (PresentationPage)pages.get(0);
+      return (pages == null || pages.size() == 0) ? null : (PresentationPage)pages.get(0);
    }
    
    
